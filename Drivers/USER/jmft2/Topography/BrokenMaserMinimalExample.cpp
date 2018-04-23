109054fe7ebc6dfca98fb40e9985b3828326598b
Jonny Foolang <j.m.f.tsang@cantab.net>
2018-04-23 17:31:38 +0000

    BrokenMaserMinimalExample - A *very* deconstructed version of my Maser drivers that doesn't work in parallel.
    db-minimal.pars - parameters file
    
    Example usage:
      mpirun -n 2 ./BrokenMaserMinimalExample db-minimal.pars
    
    The simulation is initiated by placing particles in a region using a
    CubeInsertionBoundary, which is removed immediately. After a number of
    timesteps, a SubcriticalMaserBoundary is added and activated. Two timesteps
    later, the simulation hangs, with the last messages from the
    DPMBase::computeOneTimeStep() loop being
    
    [Process: 0]: about to call checkAndDuplicatePeriodicParticles()
    [Process: 1]: about to call performGhostParticleUpdate()
    
    
    
    git-svn-id: https://svn.mercurydpm.org/SourceCode/Trunk@2804 b3179fc6-a625-46cb-afb8-a2a51a81216d

---
+++ Drivers/USER/jmft2/Topography/BrokenMaserMinimalExample.cpp
@@ -0,0 +1,184 @@
+/* BrokenMaserMinimalExampleBlasius - A simplified form of JMFT's Blasius
+ * drivers that doesn't work in parallel.
+ *
+ * Example usage: mpirun -n 2 ./BrokenMaserMinimalExampleBlasius db-minimal.pars 
+ */
+#include "Mercury2D.h"
+#include "Particles/BaseParticle.h"
+#include "Boundaries/CubeInsertionBoundary.h"
+#include "Boundaries/PeriodicBoundary.h"
+#include "Boundaries/SubcriticalMaserBoundary.h"
+#include "Species/LinearViscoelasticFrictionSpecies.h"
+#include <iostream>
+#include "Math/RNG.h"
+#include "Math/ExtendedMath.h"
+#include "File.h"
+
+class BrokenMaserMinimalExample : public Mercury2D {
+    public:
+
+        /* Constructor */
+        BrokenMaserMinimalExample(std::string parsfile)
+        {
+            /* Reading config file */
+            std::ifstream file(parsfile);
+            std::string name;
+            double var;
+            while (file >> name >> var) 
+                pars[name] = var;
+
+
+            /* Initialisation */
+            setName(parsfile.erase(parsfile.find_last_of('.')));
+            if (pars.find("randomSeed") != pars.end())
+            {
+                random.setRandomSeed(int(pars.at("randomSeed")));
+                logger(INFO, "Set random seed to %", pars.at("randomSeed"));
+            }
+            else
+            {
+                random.randomise();
+                logger(INFO, "Random seed not specified, randomising.");
+            }
+
+            dataFile.setFileType(FileType::MULTIPLE_FILES);
+            fStatFile.setFileType(FileType::NO_FILE);
+
+            setXMin(pars.at("xmin"));
+            setXMax(pars.at("xmax"));
+            setYMin(0);
+            setYMax(pars.at("reservoirHeight"));
+            setZMin(0);
+            setZMax(1);
+            setNumberOfDomains({2, 1, 1});
+
+            /* Species */
+            /* JMFT: We must set up species in the constructor, or in any case before
+             * we call setupInitialConditions(), if we want to use MPI. 
+             */
+
+            spec_particles = new LinearViscoelasticFrictionSpecies();
+            spec_particles->setDensity(1);
+            spec_particles->setCollisionTimeAndRestitutionCoefficient(
+                    pars.at("collisionTime"), 
+                    pars.at("restitutionCoefficient"),
+                    constants::pi*pow(pars.at("particleRadius"),2) // note - mass per unit _area_
+                    );
+
+            spec_particles->setSlidingFrictionCoefficient(tan( pars.at("beta") * M_PI / 180. ));
+            spec_particles->setSlidingStiffness(2.0/7.0 * spec_particles->getStiffness());
+            spec_particles->setSlidingDissipation(2.0/7.0 * spec_particles->getDissipation());
+            spec_particles->setRollingFrictionCoefficient(tan( pars.at("betaroll") * M_PI / 180. ));
+            spec_particles->setRollingStiffness(2.0/5.0 * spec_particles->getStiffness());
+            spec_particles->setRollingDissipation(2.0/5.0 * spec_particles->getDissipation());
+            spec_particles = speciesHandler.copyAndAddObject(spec_particles);
+
+            logger(INFO, "Constructor completed.");
+
+        }
+
+        ~BrokenMaserMinimalExample(void) {
+        }
+
+        void setupInitialConditions() 
+        {
+            setTimeStep(pars.at("timeStep"));
+            setTimeMax(pars.at("timeMax"));
+            setSaveCount(pars.at("saveEvery"));
+
+            /* Gravity. While filling up, let gravity point straight down. */
+            setGravity(Vec3D(0, -1, 0));
+
+            /* CubeInsertionBoundary for introducing new particles */
+            auto generandum = new BaseParticle;
+            generandum->setSpecies(spec_particles);
+            generandum->setRadius(pars.at("particleRadius"));
+            double velvar = 0;
+            insb = new CubeInsertionBoundary();
+            insb->set(
+                generandum, 0, 
+                    Vec3D(pars.at("xmin") - pars.at("reservoirLength"), 
+                          0, 0),
+                    Vec3D(pars.at("xmin"), 
+                          pars.at("reservoirHeight"), 0),
+                    Vec3D(0, - sqrt(pars.at("reservoirHeight")), 0),
+                    Vec3D(0, - sqrt(pars.at("reservoirHeight")), 0),
+                    pars.at("particleRadius"),
+                    pars.at("particleRadius")
+                );
+            insb = boundaryHandler.copyAndAddObject(insb);
+            insb->checkBoundaryBeforeTimeStep(this);
+            boundaryHandler.removeObject(insb->getId());
+
+            stillFillingUp = true;
+        }
+
+        void actionsAfterTimeStep() 
+        {
+            logger(INFO, "In actionsAfterTimeStep()");
+
+            /* Are we still filling up? If not, no need to do anything here. */
+            if (!stillFillingUp) 
+                return;
+
+            /* Have we filled the reservoir up enough already? */
+
+            if (getTime() > 20*getTimeStep())
+            {
+                /* MaserBoundary */
+                auto masb = new SubcriticalMaserBoundary();
+                masb->set(Vec3D(1.0, 0.0, 0.0), 
+                        pars.at("xmin") - pars.at("reservoirLength") + pars.at("particleRadius"), 
+                        pars.at("xmin"));
+                logger(INFO, "About to put in masb");
+                masb = boundaryHandler.copyAndAddObject(masb);
+                logger(INFO, "Have put in the masb");
+                logger(INFO, "About to activate masb");
+                masb->activateMaser();
+                logger(INFO, "Have activated masb");
+
+                /* Nothing goes wrong if we use a PeriodicBoundary instead */
+                /*
+                auto perb = new PeriodicBoundary();
+                perb->set(Vec3D(1.0, 0.0, 0.0), 
+                        pars.at("xmin") - pars.at("reservoirLength") + pars.at("particleRadius"), 
+                        pars.at("xmin"));
+                logger(INFO, "About to put in perb");
+                perb = boundaryHandler.copyAndAddObject(perb);
+                logger(INFO, "Have put in the perb");
+                */
+
+                stillFillingUp = false;
+
+            }
+            logger(INFO, "Have completed actionsAfterTimeStep");
+        }
+
+    private:
+
+        std::map<std::string,double> pars;
+        bool stillFillingUp;
+
+        CubeInsertionBoundary* insb;
+        LinearViscoelasticFrictionSpecies *spec_particles;
+
+};
+
+int main(const int argc, char* argv[]) {
+    if (argc > 1)
+    {
+        BrokenMaserMinimalExample* problem = new BrokenMaserMinimalExample(argv[1]);
+        argv[1] = argv[0];
+        problem->solve(argc-1, argv+1);
+        delete problem;
+        return 0;
+    }
+    else
+    {
+        logger(ERROR, "Usage: % config-file [options]",
+                argv[0]);
+        exit(-1);
+    }
+}
+
+
