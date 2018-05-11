daaf92c4e6720a2e022125ab3a84829ccc1b92cf
Jonny Foolang <j.m.f.tsang@cantab.net>
2018-05-11 13:46:23 +0000

    Hertzian2DSelfTest - A broken usage of the Hertzian contact model in 2D with fixed particles
    A particle that collides with a fixed particle will then have a nan position, 
    suggesting that the 'infinite mass' of fixed particles isn't being dealt with
    properly. The problem isn't there for collisions between two movables. The
    problem seems to occur for 3D as well, but I'm not sure if the elastic modulus
    has an appropriate value.
    
    
    git-svn-id: https://svn.mercurydpm.org/SourceCode/Trunk@2905 b3179fc6-a625-46cb-afb8-a2a51a81216d

---
+++ Drivers/MercurySimpleDemos/Hertzian2DSelfTest.cpp
@@ -0,0 +1,61 @@
+/* Hertzian2DUnitTest */
+#include "Mercury2D.h"
+#include "Particles/BaseParticle.h"
+#include "Species/HertzianViscoelasticFrictionSpecies.h"
+
+class Hertzian2DUnitTest : public Mercury2D
+{
+    public:
+
+        Hertzian2DUnitTest()
+        {
+
+            setName("Hertzian2DSelfTest");
+            setTimeMax(4);
+            setTimeStep(5e-5);
+            setSaveCount(200);
+
+            auto spec = new HertzianViscoelasticFrictionSpecies();
+            spec->setDensity(1);
+            spec->setElasticModulusAndRestitutionCoefficient(6e-3, 0.1);
+            spec = speciesHandler.copyAndAddObject(spec);
+
+            /* Collision between fixed and movable */
+            auto pf = new BaseParticle;
+            pf->setSpecies(spec);
+            pf->setRadius(5e-2);
+            pf->setPosition(Vec3D(0,0,0));
+            pf->fixParticle();
+            particleHandler.copyAndAddObject(pf);
+
+            auto pm = new BaseParticle;
+            pm->setSpecies(spec);
+            pm->setRadius(5e-2);
+            pm->setPosition(Vec3D(1,0,0));
+            pm->setVelocity(Vec3D(-1,0,0));
+            particleHandler.copyAndAddObject(pm);
+
+            /* Collision between two movables */
+            pm->setPosition(Vec3D(0,1,0));
+            pm->setVelocity(Vec3D(1,0,0));
+            particleHandler.copyAndAddObject(pm);
+            
+            pm->setPosition(Vec3D(1,1,0));
+            pm->setVelocity(Vec3D(0,0,0));
+            particleHandler.copyAndAddObject(pm);
+
+
+        }
+
+        ~Hertzian2DUnitTest()
+        {
+        }
+};
+
+int main() 
+{
+    auto problem = new Hertzian2DUnitTest;
+    problem->solve();
+    return 0;
+}
+
