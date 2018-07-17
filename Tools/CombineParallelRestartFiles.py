e5121aed2b5796abaa9322cbd473c7ffb6e5fc56
irana <irana@mycompany.com>
2018-07-17 11:36:44 +0000

    Adding a python script that can combine multiple restart files that are generated by the parallel code into one (regular, serial) restart file.
    git-svn-id: https://svn.mercurydpm.org/SourceCode/Trunk@3187 b3179fc6-a625-46cb-afb8-a2a51a81216d

---
+++ Tools/CombineParallelRestartFiles.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+'''
+Short script to combine restart files that are generated in the parallel code, to a single restart file with all
+particles and interactions. It is assumed that the restart files are all the same except for the particles and
+interactions.
+
+Usage:
+- In case of restart files of the form myRestartFile.restart0, myRestartFile.restart1, etc.:
+    ./CombineParallelRestartFiles.py myRestartFile.restart
+- In case of restart files of the form myRestartFile.restart0.123456, myRestartFile.restart1.123456, etc:
+    ./CombineParallelRestartFiles.py myRestartFile.restart 123456
+'''
+
+
+def main():
+    # the user gives the base of the restart-file name, we find the restart files of all the cores
+    # the argument should include the .restart!
+    restart_file_base = sys.argv[1]
+    files = sorted([file for file in os.listdir(".") if file.startswith(restart_file_base)])
+
+    # Optionally, the user can give a number s.t. all the files that end with that number will be combined. Necessary
+    # if the files are generated with FileType::MULTIPLE_FILES or FileType::MULTIPLE_FILES_PADDED
+    if len(sys.argv) > 2:
+        restart_file_number = sys.argv[2]
+        files = sorted([file for file in files if file.endswith(restart_file_number)])
+
+    print("Combining the following restart files: ")
+    print(files)
+
+    # construct the combined file
+    out_file = open("combined" + restart_file_base, 'w')
+
+    # The algorithm below is very naive, but works: first construct the header, particles, interactions and footer
+    # separately, then write them all to the new file
+    header = []
+    particles = []
+    interactions = []
+    footer = []
+    footer_written = False
+
+    # Construct the header (everything before the particles) and determine the line number where the particles start
+    file0 = open(files[0])
+    file0 = file0.readlines()
+
+    index_particles = 0
+    while 'Particles' not in file0[index_particles]:
+        header.append(file0[index_particles])
+        index_particles += 1
+
+    # For all files, collect the particle and interaction data. Also collect the footer information once.
+    for file in files:
+        # Open the file and mold it into a list of lists of all the 'words' in the file
+        file0 = open(file)
+        file0 = file0.readlines()
+        file0_split = [line.split() for line in file0]
+
+        # Collect all particles (we cannot directly write to file, as we need the number of particles)
+        index_particles_end = index_particles + int(file0_split[index_particles][1]) + 1
+        for i in range(index_particles + 1, index_particles_end):
+            particles.append(file0[i])
+
+        # Collect all interactions
+        index_interactions_end = index_particles_end + int(file0_split[index_particles_end][1]) + 1
+        for i in range(index_particles_end + 1, index_interactions_end):
+            interactions.append(file0[i])
+
+        # Collect everything below the interactions, such as h-grid info, chute line
+        if not footer_written:
+            footer_written = True
+            for i in range(index_interactions_end, len(file0_split)):
+                footer.append(file0[i])
+
+    # Finally, write everything to the combined restart file
+    out_file.write("".join(header))
+    out_file.write("Particles " + str(len(particles)) + "\n")
+    out_file.write("".join(particles))
+    out_file.write("Interactions " + str(len(interactions)) + "\n")
+    out_file.write("".join(interactions))
+    out_file.write("".join(footer))
+
+
+if __name__ == '__main__':
+    main()
