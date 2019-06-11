1ff7c60a9af066e6500cf3bdb892b22a0d561963
Thomas Weinhartt <t.weinhart@utwente.nl>
2019-06-11 08:19:04 +0000

    Cleaned up a few CMakeLists.txt files
    git-svn-id: https://svn.mercurydpm.org/SourceCode/Trunk@3758 b3179fc6-a625-46cb-afb8-a2a51a81216d

--- Configuration/MasterMake.cmake
+++ Configuration/MasterMake.cmake
@@ -1,6 +1,14 @@
+#This file is included in all CMakeLists.txt files in the Driver directory
+
+#Define includes for compiling the Driver codes
+#\todo why is binary added here?
 include_directories(${Mercury_SOURCE_DIR}/Kernel
                     ${Mercury_BINARY_DIR}/Kernel)
 
+#Part 2 : Make run test for each of the demo files
+##################################################
+
+#For MPI*Test drivers:
 #Define a function to extract the number of required processors
 function(get_number_of_cores EXECNAME NUMCORES)
   string(FIND ${EXECNAME} "MPI" POS1)
@@ -14,40 +22,33 @@ function(get_number_of_cores EXECNAME NUMCORES)
   set(NUMCORES ${NUMCORES} PARENT_SCOPE)
 endfunction()
 
-#Part 2 : Make run test for each of the demo files
-##################################################
-
 file(GLOB SELFTESTS "*SelfTest.cpp")
 file(GLOB UNITTESTS "*UnitTest.cpp")
 file(GLOB MPITESTS  "*MPI*Test.cpp")
-#for each demo add a test with the same name
+#For each demo add a test with the same name
+
+foreach (TEST ${UNITTESTS} ${SELFTESTS})
+	get_filename_component(EXECNAME ${TEST} NAME_WE)
+	add_test(${EXECNAME} ${EXECNAME})
+endforeach()
+
 if (Mercury_USE_MPI)
-	foreach (TEST ${UNITTESTS} ${SELFTESTS})
-        	get_filename_component(EXECNAME ${TEST} NAME_WE)
-        	add_test(${EXECNAME} ${EXECNAME})
-	endforeach()
 	foreach (TEST ${MPITESTS})
         	get_filename_component(EXECNAME ${TEST} NAME_WE)
             get_number_of_cores(${EXECNAME} NUMCORES)
         	add_test(${EXECNAME} mpiexec -n ${NUMCORES} ./${EXECNAME})
 	endforeach()
-
-else()
-	foreach (TEST ${UNITTESTS} ${SELFTESTS})
-		get_filename_component(EXECNAME ${TEST} NAME_WE)
-		add_test(${EXECNAME} ${EXECNAME} )
-	endforeach()
 endif()
 
-#Part 3 : Make tests for each of the selftest_data files
+#Part 3 : Make tests for each of the SelfTestData files
 ########################################################
 
 
-file(GLOB TESTDATAFILES "${CMAKE_CURRENT_SOURCE_DIR}/SelfTestData/*.*")
+file(GLOB SELFTESTDATAFILES "${CMAKE_CURRENT_SOURCE_DIR}/SelfTestData/*.*")
 file(GLOB MPITESTDATAFILES "${CMAKE_CURRENT_SOURCE_DIR}/MPITestData/*.*")
 
 #for each file in the selftest_data folder create a test. Which checks the data against this old data. The actually testing is done my the script self_test.
-foreach(TESTFILE ${TESTDATAFILES})
+foreach(TESTFILE ${SELFTESTDATAFILES})
         get_filename_component(TESTNAME ${TESTFILE} NAME)
 		add_test(${TESTNAME} ${CMAKE_SOURCE_DIR}/Scripts/self_test ${TESTFILE} ${TESTNAME})
         #Add the newly created files to the clean target
