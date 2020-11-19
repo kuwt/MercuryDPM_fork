9bf1c1bb2e3461c44693df3aedd4f44f208634e7
Thomas Weinhartt <t.weinhart@utwente.nl>
2020-11-19 22:14:32 +0000

    Updated Binary.cpp
    git-svn-id: https://svn.mercurydpm.org/SourceCode/Trunk@5082 b3179fc6-a625-46cb-afb8-a2a51a81216d

--- Drivers/Leidenfrost/Binary.cpp
+++ Drivers/Leidenfrost/Binary.cpp
@@ -30,21 +30,18 @@
 
 //#define DEBUG_OUTPUT
 
-class my_problem : public Mercury3D
+class Binary : public Mercury3D
 {
 
 public:
 
-    void setupInitialConditions()
+    void setupInitialConditions() override
     {
-
-        double particle_mass1 = 4.0 / 3.0 * particle_density1 * constants::pi * pow(particle_radius1, 3);
-
-        double particle_mass2 = 4.0 / 3.0 * particle_density2 * constants::pi * pow(particle_radius2, 3);
-
-        //0 : Walls
-        //1,2 Particles
-        //3 Base Wall
+        // defines species
+        //0 : side walls
+        //1 : particle species 1
+        //2 : particle species 2
+        //3 : base wall
         auto species0 = speciesHandler.copyAndAddObject(LinearViscoelasticSlidingFrictionSpecies());
         auto species1 = speciesHandler.copyAndAddObject(species0);
         auto species2 = speciesHandler.copyAndAddObject(species0);
@@ -55,214 +52,186 @@ public:
         auto species31 = speciesHandler.getMixedObject(species3, species1);
         auto species32 = speciesHandler.getMixedObject(species3, species2);
 
-        //Set up partilce 1 properties - Please note walls are species 0.
-        species1->setCollisionTimeAndRestitutionCoefficient(tc, r_particle11, particle_mass1);
-        species10->setCollisionTimeAndNormalAndTangentialRestitutionCoefficient(tc, r_wall, 1, 2 * particle_mass1); //note: here, tangential cor is set to 1 (i.e. no tangential friction). Can alternatively set to r_wall, for instance.
-        species10->setSlidingFrictionCoefficient(mu_wall);
+        //set densities of particle species 1 and 2
         species1->setDensity(particle_density1);
-
-        //Set up particle 2 properties
-        species2->setCollisionTimeAndRestitutionCoefficient(tc, r_particle22, particle_mass2);
-        species20->setCollisionTimeAndNormalAndTangentialRestitutionCoefficient(tc, r_wall, 1, 2 * particle_mass2); //note: here, tangential cor is set to 1 (i.e. no tangential friction). Can alternatively set to r_wall, for instance.
-        species20->setSlidingFrictionCoefficient(mu_wall);
-        species21->setCollisionTimeAndNormalAndTangentialRestitutionCoefficient(tc, r_particle12, 1, 2 * particle_mass1 * particle_mass2 / (particle_mass1 + particle_mass2));
         species2->setDensity(particle_density2);
 
-        //Set up particle 3 properties
+        //computes mass of particles of species 1 and 2, based on radius
+        double particle_mass1 = 4.0 / 3.0 * particle_density1 * constants::pi * pow(particle_radius1, 3);
+        double particle_mass2 = 4.0 / 3.0 * particle_density2 * constants::pi * pow(particle_radius2, 3);
+
+        //Set properties of particle - particle collisions (both species 1)
+        species1->setCollisionTimeAndRestitutionCoefficient(tc, r_particle11, particle_mass1);
+        //Set properties of particle - particle collisions (both species 2)
+        species2->setCollisionTimeAndRestitutionCoefficient(tc, r_particle22, particle_mass2);
+        //Set properties of particle - particle collisions (species 1 - species 2)
+        species21->setCollisionTimeAndNormalAndTangentialRestitutionCoefficient(tc, r_particle12, 1, 2 * particle_mass1 * particle_mass2 / (particle_mass1 + particle_mass2));
+
+        //Set properties of particle - side wall collisions (species 1)
+        //note: here, tangential restitution is set to 1 (i.e. no tangential dissipation). Can alternatively set to r_wall, for instance.
+        species10->setCollisionTimeAndNormalAndTangentialRestitutionCoefficient(tc, r_wall, 1, 2 * particle_mass1);
+        species10->setSlidingFrictionCoefficient(mu_wall);
+        //Set properties of particle - side wall collisions (species 1)
+        species20->setCollisionTimeAndNormalAndTangentialRestitutionCoefficient(tc, r_wall, 1, 2 * particle_mass2);
+        species20->setSlidingFrictionCoefficient(mu_wall);
+
+        //Set properties of particle - base wall collisions (species 1)
         species31->setCollisionTimeAndNormalAndTangentialRestitutionCoefficient(tc, r_base, 1, 2 * particle_mass1);
-        species32->setCollisionTimeAndNormalAndTangentialRestitutionCoefficient(tc, r_base, 1, 2 * particle_mass2);
         species31->setSlidingFrictionCoefficient(mu_wall);
+        //Set properties of particle - base wall collisions (species 2)
+        species32->setCollisionTimeAndNormalAndTangentialRestitutionCoefficient(tc, r_base, 1, 2 * particle_mass2);
         species32->setSlidingFrictionCoefficient(mu_wall);
 
-        //Make it a 3D problem
-        setParticleDimensions(3);
-        setSystemDimensions(3);
-
-        //Five solid walls - open top
+        //general wall properties
         InfiniteWall w0;
-        w0.set(Vec3D(-1.0, 0.0, 0.0),Vec3D(getXMin(),0.0,0.0));
-        w0.setPrescribedPosition([this] (double time)
-        {
-            double t = getTime()-1.0;
-            if (t > 0.0)
-            {
-                return Vec3D(getXMin(),0.0,shaker_amp * std::sin(t * 2.0 * shaker_freq * constants::pi));
-            }
-            else
-            {
-                return Vec3D(getXMin(),0.0,0.0);
-            }
+        w0.setSpecies(species3);
+
+        // set left side wall in x-direction
+        w0.set(Vec3D(-1.0, 0.0, 0.0),getMin());
+        w0.setPrescribedPosition([this] (double time) {
+            return Vec3D(getXMin(),getYMin(),getZMin()+shaker_amp *
+            std::sin(getTime() * 2.0 * shaker_freq * constants::pi));
         });
         wallHandler.copyAndAddObject(w0);
 
-        w0.set(Vec3D(+1.0, 0.0, 0.0),Vec3D(getXMax(),0.0,0.0));
-        w0.setPrescribedPosition([this] (double time)
-        {
-            double t = getTime()-1.0;
-            if (t > 0.0)
-            {
-                return Vec3D(getXMax(),0.0,shaker_amp * std::sin(t * 2.0 * shaker_freq * constants::pi));
-            }
-            else
-            {
-                return Vec3D(getXMax(),0.0,0.0);
-            }
+        // set right side wall in x-direction
+        w0.set(Vec3D(1.0, 0.0, 0.0),getMax());
+        w0.setPrescribedPosition([this] (double time) {
+            return Vec3D(getXMax(),getYMax(),getZMax()+shaker_amp *
+            std::sin(getTime() * 2.0 * shaker_freq * constants::pi));
         });
         wallHandler.copyAndAddObject(w0);
 
-        w0.set(Vec3D(0.0, -1.0, 0.0), Vec3D(0.0,getYMin(),0.0));
-        w0.setPrescribedPosition([this] (double time)
-        {
-            double t = getTime()-1.0;
-            if (t > 0.0)
-            {
-                return Vec3D(0.0,getYMin(),shaker_amp * std::sin(t * 2.0 * shaker_freq * constants::pi));
-            }
-            else
-            {
-                return Vec3D(0.0,getYMin(),0.0);
-            }
+        // set left side wall in y-direction
+        w0.set(Vec3D(0.0, -1.0, 0.0), getMin());
+        w0.setPrescribedPosition([this] (double time) {
+            return Vec3D(getXMin(),getYMin(),getZMin()+shaker_amp *
+            std::sin(getTime() * 2.0 * shaker_freq * constants::pi));
         });
         wallHandler.copyAndAddObject(w0);
 
-        w0.set(Vec3D(0.0, +1.0, 0.0), Vec3D(0.0,getYMax(),0.0));
-        w0.setPrescribedPosition([this] (double time)
-        {
-            double t = getTime()-1.0;
-            if (t > 0.0)
-            {
-                return Vec3D(0.0,getYMax(),shaker_amp * std::sin(t * 2.0 * shaker_freq * constants::pi));
-            }
-            else
-            {
-                return Vec3D(0.0,getYMax(),0.0);
-            }
+        // set right side wall in y-direction
+        w0.set(Vec3D(0.0, 1.0, 0.0),getMax());
+        w0.setPrescribedPosition([this] (double time) {
+            return Vec3D(getXMax(),getYMax(),getZMax()+shaker_amp *
+            std::sin(getTime() * 2.0 * shaker_freq * constants::pi));
         });
         wallHandler.copyAndAddObject(w0);
 
-        w0.set(Vec3D(0.0, 0.0, -1.0), Vec3D(0.0,0.0,getZMin()));
-        w0.setIndSpecies(3);
-        w0.setPrescribedPosition([this] (double time)
-        {
-            double t = getTime()-1.0;
-            if (t > 0.0)
-            {
-                return Vec3D(0.0,0.0,getZMin() + shaker_amp * std::sin(t * 2.0 * shaker_freq * constants::pi));
-            }
-            else
-            {
-                return Vec3D(0.0,0.0,getZMin());
-            }
+        // set base wall
+        w0.set(Vec3D(0.0, 0.0, -1.0),getMin());
+        w0.setPrescribedPosition([this] (double time) {
+            return Vec3D(getXMin(),getYMin(),getZMin()+shaker_amp * std::sin(getTime() * 2.0 * shaker_freq * constants::pi));
         });
         wallHandler.copyAndAddObject(w0);
-        
+
         //Put the particles on a grid with small random velocities
         SphericalParticle p0;
         double max_radius = std::max(particle_radius1, particle_radius2);
         unsigned int N = numberOfParticles;
-        double x = max_radius * 1.01;
-        double y = max_radius * 1.01;
-        double z = getZMin() + max_radius * 1.01;
+        double x = max_radius;
+        double y = max_radius;
+        double z = getZMin() + max_radius;
         for (int i = 0; i < N; i++)
         {
-            x += max_radius * 2.01;
+            x += max_radius * 2.;
             if (x > getXMax() - max_radius)
             {
-                x = max_radius * 1.01;
-                y += max_radius * 2.01;
+                x = max_radius;
+                y += max_radius * 2.;
             }
             if (y > getYMax() - max_radius)
             {
-                x = max_radius * 1.01;
-                y = max_radius * 1.01;
-                z += max_radius * 2.01;
+                x = max_radius;
+                y = max_radius;
+                z += max_radius * 2.;
             }
 
             p0.setPosition(Vec3D(x, y, z));
-            random.randomise(); //TURN THIS OFF IF I WANT REPRODUCIBLE DATA!!
             p0.setVelocity(Vec3D(random.getRandomNumber(-0.01, 0.01), random.getRandomNumber(-0.01, 0.01), random.getRandomNumber(-0.01, 0.01)));
 
-            //species one for even numbers, species 2 for odd --> should give me an initially well-mixed system!
-            if (i % 2 == 0)
-            {
+            //species one for even numbers, species 2 for odd --> should give an initially well-mixed system
+            if (i % 2 == 0) {
                 p0.setRadius(particle_radius1);
-                p0.setSpecies(speciesHandler.getObject(1));
-            }
-            else
-            {
+                p0.setSpecies(species1);
+            } else {
                 p0.setRadius(particle_radius2);
-                p0.setSpecies(speciesHandler.getObject(2));
+                p0.setSpecies(species2);
             }
             particleHandler.copyAndAddObject(p0);
         }
+        logger(INFO,"Inserted % particles",particleHandler.getNumberOfObjects());
+        // gravity
         setGravity(Vec3D(0.0, 0.0, -9.81));
-
-        particleHandler.computeAllMasses();
-
+        // write out some information about the simulation
+        logger(INFO,"Time scale oscillation: %s", 1./shaker_freq);
+        logger(INFO,"Time scale gravity: %s", 2.0*std::min(particle_radius1,particle_radius2)/getGravity().getLength());
+        logger(INFO,"Time scales collision: %s", species1->getCollisionTime(particle_mass1));
     }
 
-    void set_ParticleRadius(double pr1, double pr2)
+
+    void setParticleRadius(double pr1, double pr2)
     {
         particle_radius1 = pr1;
         particle_radius2 = pr2;
     }
 
-    void set_WallCOR(double cor)
+    void setWallRestitution(double Restitution)
     {
-        r_wall = cor;
+        r_wall = Restitution;
     }
 
-    void set_WallFriction(double mu)
+    void setWallFriction(double mu)
     {
         mu_wall = mu;
     }
 
-    void set_ParticleCOR(double c11, double c22, double c12)
+    void setParticleRestitution(double c11, double c22, double c12)
     {
         r_particle11 = c11;
         r_particle22 = c22;
         r_particle12 = c12;
     }
 
-    void set_ParticleCOR(double cor)
+    void setParticleRestitution(double Restitution)
     {
-        r_particle11 = cor;
-        r_particle12 = cor;
-        r_particle22 = cor;
+        r_particle11 = Restitution;
+        r_particle12 = Restitution;
+        r_particle22 = Restitution;
     }
 
-    void set_ParticleDensity(double density1, double density2)
+    void setParticleDensity(double density1, double density2)
     {
         particle_density1 = density1;
         particle_density2 = density2;
     }
 
-    void set_CollisionTime(double tc_in)
+    void setCollisionTime(double tc_in)
     {
         tc = tc_in;
     }
 
-    void set_NumberOfParticles(int np)
+    void setNumberOfParticles(int np)
     {
         numberOfParticles = np;
     }
 
-    void set_Frequency(double f)
+    void setFrequency(double f)
     {
         shaker_freq = f;
     }
 
-    void set_Amplitude(double a)
+    void setInitialAmplitude(double a)
     {
         shaker_amp = a;
     }
 
-    void set_BaseCOR(double cor)
+    void setBaseRestitution(double Restitution)
     {
-        r_base = cor;
+        r_base = Restitution;
     }
 
-    void set_switch_plate_amplitude(double time_, double amp_)
+    void setSwitchPlateAmplitude(double time_, double amp_)
     {
         switch_time = time_;
         switch_amp = amp_;
@@ -272,31 +241,13 @@ protected:
 
     void actionsBeforeTimeStep()
     {
-        //After t=1.0 start to move the bottom wall
-        double t = getTime();
-		///todo{DK: This is the old moving wall implementation, the new one has to be tested}
-        /*if (t > 1.0)
-        {
-            wallHandler.getObject(4)->move(getZMin() - shaker_amp * sin(t * 2.0 * shaker_freq * constants::pi));
-
-            double vel = shaker_amp * 2.0 * shaker_freq * constants::pi * cos(t * 2.0 * shaker_freq * constants::pi);
-
-            wallHandler.getObject(0)->move(Vec3D(0, 0, vel), getTimeStep());
-
-            wallHandler.getObject(1)->move(Vec3D(0, 0, vel), getTimeStep());
-
-            wallHandler.getObject(2)->move(Vec3D(0, 0, vel), getTimeStep());
-
-            wallHandler.getObject(3)->move(Vec3D(0, 0, vel), getTimeStep());
-        }*/
-
-        if (t > switch_time)
-            set_Amplitude(switch_amp);
-
+        if (getTime() > switch_time) {
+            shaker_amp = switch_amp;
+        }
     }
 
 private:
-
+    //parameters set in the main
     double particle_radius1;
     double particle_radius2;
     double particle_density1;
@@ -312,68 +263,58 @@ private:
 
     unsigned int numberOfParticles;
 
+    //when to start shaker
     double shaker_amp;
     double shaker_freq;
 
-    double switch_time;
+    //second switch
+    double switch_time=inf;
     double switch_amp;
 
 };
 
-int main(int argc UNUSED, char *argv[] UNUSED)
-{
+int main() {
 
-    //Set problem up
-    my_problem problem;
-    problem.setName("f07030");
-    problem.setTimeMax(21);
-
-    //Set Container Geometry
-    problem.setXMax(100. / 1000.);
-    //vector<int> study_num=problem.get_numbers(1,12);
-    //problem.setXMax(40.0+5.0*study_num[1]);
-
-    problem.setYMax(25. / 1000.);
-    problem.setZMax(400.0 / 1000.);
-
-    //Set Partilce and Wall properties
-    //
-    //214 5mm glass particles
-    problem.set_NumberOfParticles(250);
-    problem.set_ParticleRadius(2.50002 / 1000, 2.5 / 1000);
-    problem.set_ParticleDensity(2500, 2500); //2500 & 7850 --> glass & steel.
+    // Set up a problem of type binary
+    Binary dpm;
+    // Set name of output files
+    dpm.setName("Binary");
+    // Set simulation time
+    dpm.setTimeMax(21);
+    // Set container size
+    dpm.setMax({0.1,0.025,0.4});
+    // Set particle and wall properties (5mm glass particles)
+    // number of particles
+    dpm.setNumberOfParticles(250);
+    // radius of particles species 1/2
+    dpm.setParticleRadius(2.50002e-3, 2.5e-3);
+    // density of particles species 1/2
+    dpm.setParticleDensity(2500, 2500); //2500 & 7850 --> glass & steel.
+    // set stiffness and dissipation such that you get a constant collision time and coefficient of restitution
     double tc = 1e-4;
-    problem.set_CollisionTime(tc);
-    problem.set_WallCOR(0.7);
-    problem.set_WallFriction(0.0); //
-    problem.set_ParticleCOR(0.91, 0.91, 0.91); //(11, 22, 12) OR just leave as a single value for equal COR.
-    problem.set_BaseCOR(0.6);
-    
-    problem.set_switch_plate_amplitude(3.0, 0.862 / 1000); //(time, new amplitude)
-
-    //if (study_num[0] > 0)
-    //		{
-    //			std::cout << "Whole study has started" << std::endl;
-    //			exit(0);
-    //		}
-    //	else
-    //If the study is not complete save the data to disk and move on
-    //		{
-    //std::cout << "Going to launch a second/third/... code" <<std::endl;
-    //problem.launch_new("Param");
-    //		}
-    
-    //Shaker prop
-//This is fake lowering the frequence and raising amplitude by a factor of 5.
-
-    problem.set_Frequency(70);
-    problem.set_Amplitude(0.862 / 1000); //normally 0.862
-    //problem.set_Amplitude(2.5/1000);
-    
-    //Now run the code and solve  - time is set here because I am using the autodetect
-    problem.autoNumber();
-    problem.setTimeStep(tc / 50);
-    problem.setSaveCount(1000 * 21);
-    
-    problem.solve();
+    dpm.setCollisionTime(tc);
+    // coefficient of restitution or particle - particle collisions (for species 1-1, 1-2, and 2-2)
+    dpm.setParticleRestitution(0.91, 0.91, 0.91); //(11, 22, 12) OR just leave as a single value for equal Restitution.
+    // coefficient of restitution or particle - base collisions (for all species)
+    dpm.setBaseRestitution(0.6);
+    // coefficient of restitution or particle - side wall collisions (for all species)
+    dpm.setWallRestitution(0.7);
+    // friction coefficient of walls
+    dpm.setWallFriction(0.0); //
+    // frequency of base
+    dpm.setFrequency(70);
+    // amplitude of base
+    dpm.setInitialAmplitude(0.862e-3); //normally 0.862
+    // at a certain time, switch amplitude
+    //dpm.setSwitchPlateAmplitude(3.0, 0.862e-3); //(time, new amplitude)
+    // uncomment to number your output files (prevents old simulations from being overwritten by new simulations)
+    //dpm.autoNumber();
+    // set time step
+    dpm.setTimeStep(tc / 50);
+    // set frequency of writing output
+    dpm.setSaveCount(21000);
+    // randomise seed //TURN THIS OFF IF I WANT REPRODUCIBLE DATA!!
+    //random.randomise();
+    // start the solver
+    dpm.solve();
 }
