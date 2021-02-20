7bbb9ca2d41ff29866abf1dd6c36b634af274fe9
Thomas Weinhartt <t.weinhart@utwente.nl>
2021-02-20 10:10:46 +0000

    Warning added when setting constant restitution Part 2 of commit: Forgot to include logger.h.
    git-svn-id: https://svn.mercurydpm.org/SourceCode/Trunk@5311 b3179fc6-a625-46cb-afb8-a2a51a81216d

--- Kernel/Species/NormalForceSpecies/BaseNormalForce.cc
+++ Kernel/Species/NormalForceSpecies/BaseNormalForce.cc
@@ -23,40 +23,12 @@
 //(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 //SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef MERCURY_BASENORMALFORCE_H
-#define MERCURY_BASENORMALFORCE_H
-#include "Species/BaseForce.h"
+#include "Species/NormalForceSpecies/BaseNormalForce.h"
+#include "Logger.h"
 
-class BaseNormalForce : public BaseForce
-{
-public:
-
-    BaseNormalForce() {
-        constantRestitution_ = false;
-    }
-
-    BaseNormalForce(const BaseNormalForce& p) {
-        constantRestitution_ = p.constantRestitution_;
-    }
-
-    /*!
-     * Accesses constantRestitution_.
-     */
-    bool getConstantRestitution() const {
-        return constantRestitution_;
-    }
-
-    /*!
-     * Sets constantRestitution_.
-     */
-    void setConstantRestitution(bool constantRestitution);
-
-private:
-
-    /*!
-     * If constantRestitution_ is true, the elastic and dissipative force is multiplied by the harmonic mean mass, making restitution and collision time independent of the particle mass. This is set to false by default.
-     */
-    bool constantRestitution_;
-};
-
-#endif //MERCURY_BASENORMALFORCE_H
+void BaseNormalForce::setConstantRestitution(bool constantRestitution) {
+        constantRestitution_ = constantRestitution;
+        logger(INFO,"You are using a constant restitution contact model; make sure you understand the implications this has on the stiffness and dissipation used by the contact model!");
+        //if use setCollisionTime or setRestitutionCoefficient to set the parameters of your contact model, then these functions should be called AFTER calling setConstantRestitution.
+        //For more info, see http://docs.mercurydpm.org/Trunk/d0/db6/ConstantRestitution.html
+}
