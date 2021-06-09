c05f7e5ebfa495be83f2c6a5d2e489f2adad9551
Anthony Thornton <a.r.thornton@utwente.nl>
2021-06-09 14:07:14 +0000

    Change Mitcheal's single bead code to create a mistake wrt to teh Fr number.
    git-svn-id: https://svn.mercurydpm.org/SourceCode/Trunk@5700 b3179fc6-a625-46cb-afb8-a2a51a81216d

--- Drivers/USER/Post/SingleBead/SingleBeadCode.cpp
+++ Drivers/USER/Post/SingleBead/SingleBeadCode.cpp
@@ -15,14 +15,15 @@ class DrumRot : public Mercury3D
 
 	void setupInitialConditions()
 	{
+
 		radiusS1 = 0.0005; // 1mm diameter
 		radiusS2 = sizeRatio*radiusS1;
 
 		rhoS1 = 2500.0;
 		rhoS2 = densityRatio*rhoS1;
 
-		massS1 = 4 / 3 * constants::pi * pow(radiusS1, 3.0) * rhoS1;
-		massS2 = 4 / 3 * constants::pi * pow(radiusS2, 3.0) * rhoS2;
+		massS1 = 4.0 / 3.0 * constants::pi * pow(radiusS1, 3.0) * rhoS1;
+		massS2 = 4.0 / 3.0 * constants::pi * pow(radiusS2, 3.0) * rhoS2;
 
 		double fillVolume = drumFillFraction*constants::pi*pow(drumRadius,2.0)*(std::abs(getYMax()-getYMin()));
         numS2 = maxNrS2;
@@ -36,10 +37,10 @@ class DrumRot : public Mercury3D
 		numS2ToBeInserted = numS2;
 
 		//std::cout << " mass " << massS1 << " " << massS2 << std::endl;
+		std::cout << "fillVolume = "<<fillVolume<<std::endl;
+		std::cout << "fillFraction = "<<drumFillFraction<<std::endl;
 
-		tc = 1 / 4000.0;
-		//original value
-		//tc = 0.005;
+		tc = 5.0482e-5;//was 1/4000 //=0.005*sqrt(d/9.81)
 
 		speciesHandler.clear();
 
@@ -146,7 +147,7 @@ class DrumRot : public Mercury3D
 		wallHandler.copyAndAddObject(w0);
 
 		SphericalParticle p0;
-		double radius = 0.0;
+		//double radius = 0.0;
 		int numS1Inserted=0;
 		int numS2Inserted=0;
 		Vec3D pos;
@@ -157,7 +158,7 @@ class DrumRot : public Mercury3D
         //p0.setRadius(radiusS1);
         //R = random.getRandomNumber(2*radiusS1,drumRadius-2.0*p0.getRadius()); // Find a random radial position within the cylinder
         //C = random.getRandomNumber(0.0*constants::pi,2.0*constants::pi); // Find a random angular position within the cylinder
-                // Set the position in the cylinder with a random z-coordinate
+        // Set the position in the cylinder with a random z-coordinate
         //p0.setPosition(Vec3D(R*cos(C),random.getRandomNumber(getYMin()+p0.getRadius(),getYMax()-p0.getRadius()),R*sin(C)));
         //p0.setVelocity(Vec3D(0.0,0.0,0.0)); // Set initial particle velocity
         //particleHandler.copyAndAddObject(p0);
@@ -208,18 +209,21 @@ class DrumRot : public Mercury3D
             numS1Inserted++;
             hGridRebuild();
 		}
-		std::cout << "Number of S1 particles inserted" << numS1Inserted << std::endl;
-		std::cout << "Number of S2 particles inserted" << numS2Inserted << std::endl;
+		std::cout << "Number of S1 particles inserted: " << numS1Inserted << std::endl;
+		std::cout << "Number of S2 particles inserted: " << numS2Inserted << std::endl;
 
 		if ( (numS1Inserted == numS1) && (numS2Inserted == numS2) )
 		{
 			step = 2;
-			checkTime = getTime() + 1.0;
+			checkTime = getTime() + 0.5; //after this, rotation starts
 			std::cout << "\n \n \n";
 			std::cout << "Particles settling down, checkTime = " << checkTime << std::endl;
 			std::cout << "--------------------------" << std::endl;
 			std::cout << "\n\n\n";
 		}
+		//Write output to vtk file for Paraview
+        setParticlesWriteVTK(true);
+        setWallsWriteVTK(FileType::ONE_FILE);
 	}
 
 	void actionsBeforeTimeStep()
@@ -232,7 +236,7 @@ class DrumRot : public Mercury3D
 		{
 			if (getTime() > checkTime)
 			{
-				std::cout << "Current KE" << getKineticEnergy() << std::endl;
+				std::cout << "Current KE: " << getKineticEnergy() << std::endl;
 				if (getKineticEnergy() < (10.0))
 				{
 					step = 3;
@@ -240,13 +244,13 @@ class DrumRot : public Mercury3D
                     setSaveCount(2000);
 					double drumStartTime = getTime();
 					std::cout << "\n \n \n";
-					std::cout << "Start the drum rotation, rpm = " << revolutionsPerSecond*60.0 << std::endl;
+					std::cout << "Start the drum rotation, rpm = " << radpersecond*60/2/constants::pi << std::endl;
 					std::cout << "--------------------------" << std::endl;
 					std::cout << "\n\n\n";
 					// rotate the drum
-					wallHandler.getObject(0)->setAngularVelocity(Vec3D(0.0,revolutionsPerSecond*constants::pi*2.0,0.0));
-					wallHandler.getObject(1)->setAngularVelocity(Vec3D(0.0,revolutionsPerSecond*constants::pi*2.0,0.0));
-					wallHandler.getObject(2)->setAngularVelocity(Vec3D(0.0,revolutionsPerSecond*constants::pi*2.0,0.0));
+					wallHandler.getObject(0)->setAngularVelocity(Vec3D(0.0,radpersecond,0.0));
+					wallHandler.getObject(1)->setAngularVelocity(Vec3D(0.0,radpersecond,0.0));
+					wallHandler.getObject(2)->setAngularVelocity(Vec3D(0.0,radpersecond,0.0));
 
 					//wallHandler.getObject(0)->setOrientation(Vec3D(0.0,1.0,0.0));
 					//wallHandler.getObject(1)->setOrientation(Vec3D(0.0,-1.0,0.0));
@@ -267,13 +271,13 @@ class DrumRot : public Mercury3D
 	    setSaveCount(2000);
 
 	    std::cout << "\n \n \n";
-		std::cout << "Set drum rotation speed (rpm) to " << revolutionsPerSecond*60.0 << std::endl;
+		std::cout << "Set drum rotation speed (rpm) to " << radpersecond*60/2/constants::pi << std::endl;
 		std::cout << "--------------------------" << std::endl;
 		std::cout << "\n\n\n";
 
-        wallHandler.getObject(0)->setAngularVelocity(Vec3D(0.0,revolutionsPerSecond*constants::pi*2.0,0.0)); //rad/s
-		wallHandler.getObject(1)->setAngularVelocity(Vec3D(0.0,revolutionsPerSecond*constants::pi*2.0,0.0));
-		wallHandler.getObject(2)->setAngularVelocity(Vec3D(0.0,revolutionsPerSecond*constants::pi*2.0,0.0));
+        wallHandler.getObject(0)->setAngularVelocity(Vec3D(0.0,radpersecond,0.0)); //rad/s
+		wallHandler.getObject(1)->setAngularVelocity(Vec3D(0.0,radpersecond,0.0));
+		wallHandler.getObject(2)->setAngularVelocity(Vec3D(0.0,radpersecond,0.0));
 	}
 
 	void setDrumRadius (double radius)
@@ -281,9 +285,10 @@ class DrumRot : public Mercury3D
 		drumRadius = radius;
 	}
 
-	void setRevolutionSpeed (double rpm)
+	///Set the reveolution speed in rad per second.
+	void setRevolutionSpeed (double rotRate) //rotRate from main is used here
 	{
-		revolutionsPerSecond = rpm/60.0;// non-dimensionalised based on 3 mm particles and g=9.81
+		radpersecond = rotRate;
 	}
 
 	void setSizeAndDensityRatio (double sr, double dr)
@@ -369,7 +374,7 @@ class DrumRot : public Mercury3D
 	int numS1ToBeInserted,numS2ToBeInserted;
 
 	double drumRadius;
-	double revolutionsPerSecond;
+	double radpersecond;
 	double fractionalPolydispersity;
 
 	double slidingFrictionDrum;
@@ -393,8 +398,8 @@ int main(int argc, char *argv[])
 {
 
     // Start measuring elapsed time
-    //std::chrono::time_point<std::chrono::system_clock> startClock, endClock;
-    //startClock = std::chrono::system_clock::now();
+    std::chrono::time_point<std::chrono::system_clock> startClock, endClock;
+    startClock = std::chrono::system_clock::now();
 
 	DrumRot problem;
 
@@ -403,7 +408,7 @@ int main(int argc, char *argv[])
 	//the name of the file
 	//*******************Excitation Properties*****************************************
 	//Drum rotation rate (rpm)
-	double rotRateBasal = 1.0;
+	double rotRateBasal = 1.0; //not used at all?
 
 	//*******************Frictional Properties*****************************************
 	//sliding friction for species 1, 2 and wall
@@ -430,7 +435,7 @@ int main(int argc, char *argv[])
     // computes rpm based on froude number and drumRad
     problem.setTimeMax(150.0);
 	problem.setTimeStep(1.0/(4000.0 * 50.0));
-	double froudeNumber = 0.22;
+	double froudeNumber = 2.3;
 	double rotRate = pow(froudeNumber*9.81/drumRad,0.5);//*(60.0/(2.0*3.1415926535));
     std::cout<< "rotRate = " << rotRate << std::endl;
 	//Set the number of domains for parallel decomposition
@@ -459,12 +464,12 @@ int main(int argc, char *argv[])
 
     if (argc>1)
     {
-        //std::stringstream nameStream;
-        //std::string fullName = nameStream.str();
-        //fullName = argv[1];
-        //std::cout << "restarting from " << fullName << std::endl;
-        //problem.readRestartFile(fullName);
-        //problem.setAppend(true);
+        std::stringstream nameStream;
+        std::string fullName = nameStream.str();
+        fullName = argv[1];
+        std::cout << "restarting from " << fullName << std::endl;
+        problem.readRestartFile(fullName);
+        problem.setAppend(true);
     }
 	//problem.random.randomise();
 
@@ -492,17 +497,28 @@ int main(int argc, char *argv[])
 	problem.setRollingFriction(muRWall,muR1,muR2); //wall, species1, species2
 	problem.setTorsionFriction(muTWall,muT1,muT2); //wall, species1, species2
 
-	problem.setRevolutionSpeed(rotRate);//rpm
+	problem.setRevolutionSpeed(rotRate);//rad per second
 
 	problem.setSaveCount(2000);
 	problem.readArguments(argc,argv);
 
-    problem.dataFile.setFileType(FileType::MULTIPLE_FILES);
+    problem.dataFile.setFileType(FileType::ONE_FILE);
     problem.restartFile.setFileType(FileType::ONE_FILE);
     problem.fStatFile.setFileType(FileType::NO_FILE);
     problem.eneFile.setFileType(FileType::NO_FILE);
 
+	//Set the number of domains for parallel decomposition
+	//problem.setNumberOfDomains({3,2,3});
+
+    //problem.setNumberOfOMPThreads(72);
+
 	problem.solve();
 
+    std::cout << "\n";
+    std::cout <<" "<<problem.getName();
+    std::cout << "\n";
+    endClock = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = endClock - startClock;
+    logger(INFO, "Elapsed time for solving the PDE: % s", elapsed_seconds.count());
 	return 0;
 }
