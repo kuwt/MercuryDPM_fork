9f2c0c1d4fb976118ac4dd0717e2d3980af47b40
Anthony Thornton <a.r.thornton@utwente.nl>
2021-07-07 17:07:20 +0000

    Cleaning up the scripts and making sure all data is saved in the same place
    git-svn-id: https://svn.mercurydpm.org/SourceCode/Trunk@5758 b3179fc6-a625-46cb-afb8-a2a51a81216d

--- Drivers/Segregation/Scripts/Analyse.sh
+++ Drivers/Segregation/Scripts/Analyse.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #Step 1 create (build) and copy in the centerofmass code
-echo Creating centerofmass of mass code
+echo
+echo
+echo Creating centerofmass of mass code and fstatistics...
+echo
+echo
+
 (cd ../../MercuryCG && make  centerofmass && make fstatistics)
 mkdir -p bin
 cp ../../MercuryCG/centerofmass bin/
@@ -12,20 +17,46 @@ mkdir -p stat
 cd stat
 #for num in 1 5 7 9; do
 #for num in 2 3; do
+
+echo
+echo
+echo Starting centreofmass calculation ...
+echo
+echo
+sleep 10s
+
 for num in 1; do
 #for num in 1 4 6 8 10; do
-	restartfile=$(ls ../../Segregation.$num.restart)
-  ../bin/centerofmass ../../Segregation.$num 0.0003001
+  ../bin/centerofmass ../../Segregation.$num 0.5
+  mv ../../Segregation.$num.com .
+
+echo
+echo
+echo Starting global stats ...
+echo
+echo
+sleep 10s
 
 	../bin/fstatistics ../../Segregation.$num -stattype Z \
-	 -n 200 -tmin 90 -w 0.0003 -z -0.002 0.016 -StressTypeForFixedParticles 1
+	 -n 200 -tmin 90 -w 0.5 -z 0 100 -StressTypeForFixedParticles 1 \
+	 -o Segregation.$num.stat
+echo
+echo
+echo Starting large particle stats ...
+echo
+echo
 	sleep 10s
 	 ../bin/fstatistics ../../Segregation.$num -stattype Z \
-	 -n 200 -tmin 90 -w 0.0003 -z -0.002 0.016 -StressTypeForFixedParticles 1 \
-	 -rmin 0.0003001 -o Segregation.$num.large.stat
+	 -n 200 -tmin 90 -w 0.5 -z 0 100 -StressTypeForFixedParticles 1 \
+	 -rmin 0.5 -o Segregation.$num.large.stat
+echo
+echo
+echo Starting small particle stats ....
+echo
+echo
 	sleep 10s
 	../bin/fstatistics ../../Segregation.$num -stattype Z \
-	 -n 200 -tmin 90 -w 0.0003 -z -0.002 0.016 -StressTypeForFixedParticles 1 \
-	 -rmax 0.0003001 -o Segregation.$num.small.stat
-	sleep 10s
+	 -n 200 -tmin 90 -w 0.5 -z 0 100 -StressTypeForFixedParticles 1 \
+	 -rmax 0.5 -o Segregation.$num.small.stat
+
 done
