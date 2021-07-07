c72fcb7070b2ab8dcdc11e0c45aff27aa528ee82
Anthony Thornton <a.r.thornton@utwente.nl>
2021-07-07 14:14:05 +0000

    Adding this missing stress type line
    git-svn-id: https://svn.mercurydpm.org/SourceCode/Trunk@5757 b3179fc6-a625-46cb-afb8-a2a51a81216d

--- Drivers/Segregation/Scripts/Analyse.sh
+++ Drivers/Segregation/Scripts/Analyse.sh
@@ -18,14 +18,14 @@ for num in 1; do
   ../bin/centerofmass ../../Segregation.$num 0.0003001
 
 	../bin/fstatistics ../../Segregation.$num -stattype Z \
-	 -n 200 -tmin 90 -w 0.0003 -z -0.002 0.016
+	 -n 200 -tmin 90 -w 0.0003 -z -0.002 0.016 -StressTypeForFixedParticles 1
 	sleep 10s
 	 ../bin/fstatistics ../../Segregation.$num -stattype Z \
-	 -n 200 -tmin 90 -w 0.0003 -z -0.002 0.016  \
+	 -n 200 -tmin 90 -w 0.0003 -z -0.002 0.016 -StressTypeForFixedParticles 1 \
 	 -rmin 0.0003001 -o Segregation.$num.large.stat
 	sleep 10s
 	../bin/fstatistics ../../Segregation.$num -stattype Z \
-	 -n 200 -tmin 90 -w 0.0003 -z -0.002 0.016 \
+	 -n 200 -tmin 90 -w 0.0003 -z -0.002 0.016 -StressTypeForFixedParticles 1 \
 	 -rmax 0.0003001 -o Segregation.$num.small.stat
 	sleep 10s
 done
