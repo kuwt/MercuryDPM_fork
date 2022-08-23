86a92677daac2bab1c597fef047226765650dd49
Timo Plath <t.plath@utwente.nl>
2022-08-23 13:06:35 +0000

    Merged in bugfix/MDPM-256-g-version-5-need-but-cmake-only (pull request #19)
    Bugfix/MDPM-256 g version 5 need but cmake only
    
    * Merged in master (pull request #18)
    
    Adding an ignore which has been lost in the more from svn
    
    * Merged mercurydpm/mercurydpm into master
    * Adding an ignore which has been lost in the more from svn
    
    
    Approved-by: Timo Plath
    Approved-by: Thomas Weinhart
    * MDPM-256 deleted current checks for gcc version 4.9 and 4.8 and replaced it with a check for version 5.0.
    
    * MDPM-256 changed current check for C++ version from supporting C++11 and C++14 to C++14 only.
    
    * Merged 1.0.Alpha into master
    * Merged master into bugfix/MDPM-256-g-version-5-need-but-cmake-only
    
    Approved-by: Anthony Thornton
    Approved-by: Igor Ostanin
    Approved-by: Thomas Weinhart

--- CMakeModules/MercuryCppFeatureCheck.cmake
+++ CMakeModules/MercuryCppFeatureCheck.cmake
@@ -1,26 +1,18 @@
-#Here is the check for CX11 support
+#Here is the check for CX14 support
 include(CheckCXXCompilerFlag)
 CHECK_CXX_COMPILER_FLAG("-std=c++14" COMPILER_SUPPORTS_CXX14)
-CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
 
 #Add the flag to compiler options if the current compiler is found
 if (COMPILER_SUPPORTS_CXX14)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
-elseif (COMPILER_SUPPORTS_CXX11)
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-else()	
-	message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. We require a compiler that has :support for CXX11, for example GCC 4.8 or later")
+else()
+    message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++14 support. We require a compiler that has :support for CXX14, for example GCC 5.0 or later")
 endif()
 
 if (CMAKE_COMPILER_IS_GNUCC)
-    if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9)
-        message("GCC 4.x does not support the DEPRECATED attribute; therefore, compiler warnings about setting the attribute are ignored")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-attributes")
-    endif()
-
     #Extra check for GCC if it is correct version or not
-    if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.8)
-		message(FATAL_ERROR "Insufficient gcc version, you need at least version 4.8 of GCC")
+    if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.0)
+		message(FATAL_ERROR "Insufficient gcc version, you need at least version 5.0 of GCC")
     endif()
 
 endif()
