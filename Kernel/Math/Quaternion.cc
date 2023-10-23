fe2eaf11aba6a6b8a7aab1a3fae30557dd0e1d49
Thomas Weinhart <t.weinhart@utwente.nl>
2023-10-23 09:20:32 +0000

    bugfix: Selftests pass on linux (msm3), but not on the mac.
    * Selftests pass on linux (msm3), but not on the mac. After lots of testing I found that the problem is in computing the normal direction from a quaternion. I had to replace:
    
    Vec3D Quaternion::getAxis() const
    {
        return Vec3D(q0 * q0 - q3 * q3 + q1 * q1 - q2 * q2, 2.0 * (q1 * q2 + q3 * q0), 2.0 * (q1 * q3 - q2 * q0));
    }
    
    by
    
    Vec3D Quaternion::getAxis() const
    {
        const Mdouble q00 = q0*q0;
        const Mdouble q11 = q1*q1;
        const Mdouble q22 = q2*q2;
        const Mdouble q33 = q3*q3;
        return Vec3D(q00 - q33 + q11 - q22, 2.0 * (q1 * q2 + q3 * q0), 2.0 * (q1 * q3 - q2 * q0));
    }
    
    Merged in bugfix/macos_issue (pull request #135)
    
    Approved-by: Timo Plath
    Approved-by: Igor Ostanin

--- Kernel/Math/Quaternion.cc
+++ Kernel/Math/Quaternion.cc
@@ -500,8 +500,11 @@ void Quaternion::setAngleZ(Mdouble psi)
 
 Vec3D Quaternion::getAxis() const
 {
-    //logger(ERROR,"o % d % d %",*this,Vec3D(1-2.0*q2*q2-2.0*q3*q3, 2.0*(q1*q2+q3*q0), 2.0*(q1*q3-q2*q0)),Vec3D(q0*q0+q1*q1-q2*q2-q3*q3, 2.0*(q1*q2+q3*q0), 2.0*(q1*q3-q2*q0)));
-    return Vec3D(q0 * q0 - q3 * q3 + q1 * q1 - q2 * q2, 2.0 * (q1 * q2 + q3 * q0), 2.0 * (q1 * q3 - q2 * q0));
+    const Mdouble q00 = q0*q0;
+    const Mdouble q11 = q1*q1;
+    const Mdouble q22 = q2*q2;
+    const Mdouble q33 = q3*q3;
+    return Vec3D(q00 - q33 + q11 - q22, 2.0 * (q1 * q2 + q3 * q0), 2.0 * (q1 * q3 - q2 * q0));
 }
 
 //retrieves the rotation matrix, often called A in literature.
